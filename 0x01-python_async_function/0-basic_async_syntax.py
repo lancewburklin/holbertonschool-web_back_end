1d827ce004b5db2b1682a299d63954a380f9bc65
lancewburklin <1677@holbertonschool.com>
2021-03-16 12:12:19 -0500

    Adding function annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,8 +4,8 @@ import asyncio
 from random import uniform
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """ Waits a random amount between 0 and delay """
-    num = uniform(0, max_delay)
+    num: float = uniform(0, max_delay)
     await asyncio.sleep(num)
     return num
