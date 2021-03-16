4cf9744e51f8f52cb92da25093aa3d4988e2f2f1
lancewburklin <1677@holbertonschool.com>
2021-03-16 14:21:20 -0500

    Fixed annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,9 +7,9 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ One result at a time, concurrently """
-    res: List = []
+    res: List[float] = []
     t = max_delay
     for c in asyncio.as_completed([wait_random(t) for i in range(0, n)]):
         item = await c
