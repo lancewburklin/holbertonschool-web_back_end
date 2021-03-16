230d67e4f60b57638457912b0c7a8fcbd3b15dcb
lancewburklin <1677@holbertonschool.com>
2021-03-16 14:19:25 -0500

    Added annotations

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,12 +3,13 @@
 Returning wait times from wait_random
 """
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List:
     """ One result at a time, concurrently """
-    res = []
+    res: List = []
     t = max_delay
     for c in asyncio.as_completed([wait_random(t) for i in range(0, n)]):
         item = await c
