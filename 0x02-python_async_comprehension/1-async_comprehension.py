a6d596a97722be323dca9e843ede1fee34e66173
lancewburklin <1677@holbertonschool.com>
2021-03-17 12:26:53 -0500

    Simplified comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """ Collecting generator results """
-    result: List = []
-    async for i in async_generator():
-        result.append(i)
+    result: List = [i async for i in async_generator()]
     return result
