659326b89f049093eb5c6c693db5f2a099ffc10f
lancewburklin <1677@holbertonschool.com>
2021-03-17 12:45:39 -0500

    Changed format

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,13 +2,14 @@
 """ Measuring the runtime of async_comp """
 import time
 import asyncio
-a = __import__('1-async_comprehension').async_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """ Run the generator asyncrinously and measure the runtime """
     t0 = time.time()
-    await asyncio.gather(a(), a(), a(), a())
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
     t1 = time.time()
     res: float = t1 - t0
     return (t1 - t0)
