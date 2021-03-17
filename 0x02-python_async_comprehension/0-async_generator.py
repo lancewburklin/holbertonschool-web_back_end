4a71b6536fa85c54c12a6f8c65688d01cec970a8
lancewburklin <1677@holbertonschool.com>
2021-03-17 12:11:58 -0500

    Changing return type to float

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,12 @@
 """ Creating random numbers """
 import asyncio
 from random import uniform
+from typing import Generator
 
 
-async def async_generator() -> None:
+async def async_generator() -> Generator[float, None, None]:
     """ Creating an async generator """
-    for i in range(0, 10):
+    for i in range(10):
         await asyncio.sleep(1)
-        yield uniform(0, 10)
+        num: float = uniform(0, 10)
+        yield num
