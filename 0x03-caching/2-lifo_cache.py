7e91f3e1e2daed914a809310136dc931fcd6afca
lancewburklin <1677@holbertonschool.com>
2021-03-19 15:49:34 -0500

    Refactored code, fixed bug

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -23,6 +23,15 @@ class LIFOCache(BaseCaching):
         if key is None or item is None:
             pass
         else:
+            things = list(self.cache_data.keys())
+            if key in things:
+                for lock, val in self.order.items():
+                    if val > self.order[key]:
+                        self.order[lock] = val - 1
+                num = max(list(self.order.values())) + 1
+                self.order[key] = num
+                self.cache_data[key] = item
+                return
             if len(self.cache_data) < BaseCaching.MAX_ITEMS:
                 self.cache_data[key] = item
                 if len(self.order) == 0:
@@ -31,26 +40,18 @@ class LIFOCache(BaseCaching):
                     num = max(list(self.order.values())) + 1
                     self.order[key] = num
             else:
-                items = list(self.cache_data.keys())
-                if key in items:
-                    self.cache_data[key] = item
-                    for lock, val in self.order.items():
-                        if val > self.order[key]:
-                            self.order[lock] -= 1
-                    self.order[key] = self.MAX_ITEMS
-                else:
-                    temp = None
-                    for lock, val in self.order.items():
-                        if val == self.MAX_ITEMS:
-                            print('DISCARD: ' + lock)
-                            del self.cache_data[lock]
-                            temp = lock
-                        else:
-                            self.order[lock] = val - 1
-                    if temp:
-                        del self.order[temp]
-                    self.order[key] = self.MAX_ITEMS
-                    self.cache_data[key] = item
+                temp = None
+                for lock, val in self.order.items():
+                    if val == self.MAX_ITEMS:
+                        print('DISCARD: ' + lock)
+                        del self.cache_data[lock]
+                        temp = lock
+                    else:
+                        self.order[lock] = val - 1
+                if temp:
+                    del self.order[temp]
+                self.order[key] = self.MAX_ITEMS
+                self.cache_data[key] = item
 
     def get(self, key):
         """
