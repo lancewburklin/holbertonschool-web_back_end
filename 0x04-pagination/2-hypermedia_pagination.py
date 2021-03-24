d0f2decb9b8b1abe589553b0019570850b56af61
lancewburklin <1677@holbertonschool.com>
2021-03-24 13:35:22 -0500

    Adding comments and code refactor

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
+"""
+Simple pagination list
+"""
 import csv
 import math
 from typing import List, Dict
 index_range = __import__('0-simple_helper_function').index_range
-"""
-Simple pagination list
-"""
 
 
 class Server:
@@ -13,6 +14,7 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """ Initialize function """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
@@ -33,20 +35,13 @@ class Server:
         assert page > 0
         assert page_size > 0
         data: List = []
-        items = index_range(page, page_size)
+        items: Tuple = (index_range(page, page_size))
         allData = self.dataset()
-        if (items[0] > len(allData)):
-            return data
-        if (items[1] > len(allData)):
-            items[1] = len(allData)
-        i = items[0]
-        while i < items[1]:
-            onePage = []
-            while len(onePage) != page_size:
-                onePage.append(allData[i])
-                i += 1
-            data.append(onePage)
-        return data[0]
+        i: int = items[0]
+        while i != items[1] and i < len(allData):
+            data.append(allData[i])
+            i += 1
+        return data
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """ Getting pages with additional info """
