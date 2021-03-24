617146605f741da10ad7e425b89a59ddc023a997
lancewburklin <1677@holbertonschool.com>
2021-03-24 12:34:35 -0500

    Addind docstring

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -6,5 +6,6 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple:
+	""" Returning page info """
     info: Tuple = ((page * page_size) - page_size, (page_size * page))
     return info
