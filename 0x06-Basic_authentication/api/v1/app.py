18b3001043a0bd8e9dcb1e9265f2dc5a478bf9b1
lancewburklin <1677@holbertonschool.com>
2021-03-30 16:41:59 -0500

    Setting up BasicAuth

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -14,8 +14,12 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 if getenv('AUTH_TYPE'):
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+    if getenv('AUTH_TYPE') == "basic_auth":
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
+    else:
+        from api.v1.auth.auth import Auth
+        auth = Auth()
 
 
 @app.errorhandler(404)
---
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Authorizing users """
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Basic Auth class """
+    pass
