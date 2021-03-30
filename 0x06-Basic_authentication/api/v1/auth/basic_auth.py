ef1973fd505f7167b7ecc2c566b3b18a74b3fafc
lancewburklin <1677@holbertonschool.com>
2021-03-30 16:54:28 -0500

    Adding base auth

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,12 @@ from .auth import Auth
 
 class BasicAuth(Auth):
     """ Basic Auth class """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ Extract the base64 """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if "Basic " in authorization_header:
+            return authorization_header.replace("Basic ", "")
+        return None
