9d2690625263ca8876fb3b5aa0f10196b2d24d43
lancewburklin <1677@holbertonschool.com>
2021-03-30 13:58:34 -0500

    Testing auth

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,28 @@ class Auth():
     """ Authorization class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Require Authentication """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        if len(excluded_paths) == 0:
+            return True
+        path2 = list(path)
+        if path2[0] == '/':
+            del path2[0]
+        if path2[-1] == '/':
+            del path2[-1]
+        path = "".join(path2)
+        for i in range(len(excluded_paths)):
+            temp = list(excluded_paths[i])
+            if temp[0] == '/':
+                del temp[0]
+            if temp[-1] == '/':
+                del temp[-1]
+            excluded_paths[i] = "".join(temp)
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Authorization Header """
