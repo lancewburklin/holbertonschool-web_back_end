836d714509732403ad14c88f1549a7740528b93c
lancewburklin <1677@holbertonschool.com>
2021-03-30 17:31:31 -0500

    Adding base64 decoder

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """ Authorizing users """
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """ Basic Auth class """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """ Extract the base64 """
         if authorization_header is None:
             return None
@@ -14,3 +16,19 @@ class BasicAuth(Auth):
         if "Basic " in authorization_header:
             return authorization_header.replace("Basic ", "")
         return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ Decode base64 """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            if base64.b64decode(base64_authorization_header):
+                pass
+            pass
+        except Exception:
+            return None
+        return base64.b64decode(base64_authorization_header).decode('utf-8')
