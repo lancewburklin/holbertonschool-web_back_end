cf627d39a3d04eb57bbdbaba6aeb13bf59cbe5b5
lancewburklin <1677@holbertonschool.com>
2021-03-30 12:21:19 -0500

    Adding forbidden access route

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauth():
     """ Unauthorized access """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def fail():
+    """ Forbidden access """
+    abort(403)
