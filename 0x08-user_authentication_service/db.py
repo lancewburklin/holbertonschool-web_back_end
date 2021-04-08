198b0d0329a624ea2f044afe32dfc4e28448c9f1
lancewburklin <1677@holbertonschool.com>
2021-04-08 14:31:22 -0500

    Setting up the database

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Creating the Database """
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from user import Base
+
+
+class DB:
+    """ DB class """
+    def __init__(self):
+        """ Initialize the class """
+        self._engine = create_engine("sqlite:///a.db")
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str):
+        """ Add a user to the Database """
+        from user import User
+        newUser = User()
+        newUser.email = email
+        newUser.hashed_password = hashed_password
+        self._session.add(newUser)
+        self._session.commit()
+        return newUser
