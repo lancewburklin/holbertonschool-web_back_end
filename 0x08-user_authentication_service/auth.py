269fc80f3927927784d9197bdc04e8bca2f40f24
lancewburklin <1677@holbertonschool.com>
2021-04-14 15:36:54 -0500

    Adding session id to user

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -47,3 +47,13 @@ class Auth:
             return False
         except Exception:
             return False
+
+    def create_session(self, email: str) -> str:
+        """ Create the session """
+        try:
+            per = self._db.find_user_by(email=email)
+        except Exception:
+            return None
+        new_id = _generate_uuid()
+        self._db.update_user(per.id, session_id=new_id)
+        return new_id
