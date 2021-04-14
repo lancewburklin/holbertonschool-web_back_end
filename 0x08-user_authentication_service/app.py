80fb17072480e04f2b6ee07b8e20dee119cfb848
lancewburklin <1677@holbertonschool.com>
2021-04-14 15:06:31 -0500

    Flask user creation

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
 """ Flask application """
 from flask import Flask
-from flask import jsonify
+from flask import jsonify, request
+from auth import Auth
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def basic_route():
     """ Basic route test """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    """ Create the user """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        new_user = AUTH.register_user(email, password)
+        val = {"email": "{}".format(email), "message": "user created"}
+        return jsonify(val)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
