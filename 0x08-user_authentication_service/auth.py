19d00115c6fc7717a1440a5d24961e653471e82e
lancewburklin <1677@holbertonschool.com>
2021-04-15 14:48:48 -0500

    Changed type annotation

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -68,12 +68,11 @@ class Auth:
         except Exception:
             return None
 
-    def destroy_session(self, user_id: int) -> None:
+    def destroy_session(self, user_id: str) -> None:
         """ End the session """
         try:
             per = self._db.find_user_by(id=user_id)
-            info = {"session_id": None}
-            self._db.update_user(user_id, info)
+            self._db.update_user(user_id, session_id=None)
             return None
         except NoResultFound:
             return None
