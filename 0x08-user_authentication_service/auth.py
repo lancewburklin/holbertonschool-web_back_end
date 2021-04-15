d11b86e32d84308bb36946b0f079b63086e21ef7
lancewburklin <1677@holbertonschool.com>
2021-04-15 14:26:17 -0500

    Confirm user exists

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -70,5 +70,9 @@ class Auth:
 
     def destroy_session(self, user_id: int) -> None:
         """ End the session """
-        self._db.update_user(user_id, session_id=None)
-        return None
+        try:
+            per = sef._db.find_user_by(id=user_id)
+            self._db.update_user(user_id, session_id=None)
+            return None
+        except Exception:
+            return None
