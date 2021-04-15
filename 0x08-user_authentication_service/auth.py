4a9350a0241c0aa5b9d7c180ffcec10ca8ad0f45
lancewburklin <1677@holbertonschool.com>
2021-04-15 14:55:46 -0500

    Grasping at more straws

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -68,12 +68,11 @@ class Auth:
         except Exception:
             return None
 
-    def destroy_session(self, user_id: str) -> None:
+    def destroy_session(self, user_id: int) -> None:
         """ End the session """
         try:
             per = self._db.find_user_by(id=user_id)
-            info = {"session_id": None}
-            self._db.update_user(per.id, **info)
+            self._db.update_user(per.id, session_id=None)
             return None
         except NoResultFound:
             return None
