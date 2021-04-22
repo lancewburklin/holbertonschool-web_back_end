dfa52bd286c18007aeaa1ce51c20648bc67565bd
lancewburklin <1677@holbertonschool.com>
2021-04-22 11:18:19 -0500

    Removing GET method

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def basic_route():
     """ Basic route to index """
     return render_template('0-index.html')
