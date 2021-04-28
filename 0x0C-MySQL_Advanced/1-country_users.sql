dd33cf170250530262141effc8497d22dcf6deb1
lancewburklin <1677@holbertonschool.com>
2021-04-28 11:04:20 -0500

    Initial commit

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,3 @@
+-- Creating a table
+-- Users table with country
+CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255), country ENUM('US', 'CO', 'TN') NOT NULL);
---
+++ 0x0C-MySQL_Advanced/README.md
@@ -0,0 +1,1 @@
+Using some SQL database
