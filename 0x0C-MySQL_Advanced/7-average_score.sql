cd158aaecd9c8762e29d0d8b18356cebf8732aa2
lancewburklin <1677@holbertonschool.com>
2021-04-29 14:15:52 -0500

    Getting average score

---
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,9 @@
+-- Get Average score
+-- For users
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+SET @num2 = (SELECT AVG(score) FROM corrections WHERE user_id = user_id);
+UPDATE users SET average_score = (@num2) WHERE id = user_id;
+END $$
+DELIMITER ;
