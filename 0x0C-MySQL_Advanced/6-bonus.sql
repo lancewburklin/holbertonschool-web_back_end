c91c901b43366894c1b7c230d12c1bf2a7bfc3e7
lancewburklin <1677@holbertonschool.com>
2021-04-29 13:16:28 -0500

    Adding a procedure for projects

---
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,13 @@
+-- Creating a
+-- Stored prcedure
+DELIMITER $$
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+    SET @project_id = (SELECT id FROM projects WHERE name = project_name);
+    IF (@project_id IS NULL) THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SET @project_id = (SELECT id FROM projects WHERE name = project_name);
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, @project_id, score);
+END $$
+DELIMITER ;
