fb4f774dd5372e589ee1ea640bccc3245ebf1ec3
lancewburklin <1677@holbertonschool.com>
2021-04-29 14:22:25 -0500

    Specified user_id location

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -3,7 +3,7 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-SET @num2 = (SELECT AVG(score) FROM corrections WHERE user_id = user_id);
+SET @num2 = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id);
 UPDATE users SET average_score = (@num2) WHERE id = user_id;
 END $$
 DELIMITER ;
