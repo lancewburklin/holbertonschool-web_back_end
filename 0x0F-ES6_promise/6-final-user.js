6117c8adbb37a2d2640680a5c9f2f68ee311e416
lancewburklin <1677@holbertonschool.com>
2021-05-11 16:30:17 -0500

    Build result array

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -4,5 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const first = signUpUser(fileName, lastName);
   const second = uploadPhoto(fileName);
-  return Promise.allSettled([first, second]).then((values) => values);
+  return Promise.allSettled([first, second]).then((values) => {
+    const reArray = [];
+    values.forEach((value) => {
+      reArray.push({ status: value.status, value: value.value });
+    });
+  });
 }
