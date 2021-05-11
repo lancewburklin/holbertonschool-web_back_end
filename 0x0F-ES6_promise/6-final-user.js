5e97e623e6466a11a082300c806a1e5f3cc6153d
lancewburklin <1677@holbertonschool.com>
2021-05-11 16:42:36 -0500

    Fixing return

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const first = signUpUser(fileName, lastName);
+  const first = signUpUser(firstName, lastName);
   const second = uploadPhoto(fileName);
   return Promise.allSettled([first, second]).then((values) => {
     const reArray = [];
