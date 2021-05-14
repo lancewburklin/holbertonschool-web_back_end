bc28e4b78357ad73409004affd83ffbb01c845f0
lancewburklin <1677@holbertonschool.com>
2021-05-13 20:40:57 -0500

    Override the toString method

---
+++ 0x10-ES6_classes/7-airport.js
@@ -0,0 +1,30 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+  set name(newName) {
+    if (typeof (newName) !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get name() {
+    return this.name;
+  }
+
+  set code(newCode) {
+    if (typeof (newCode) !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    this._code = newCode;
+  }
+
+  get code() {
+    return this._code;
+  }
+  toString() {
+    return `[object ${this.code}]`;
+  }
+}
