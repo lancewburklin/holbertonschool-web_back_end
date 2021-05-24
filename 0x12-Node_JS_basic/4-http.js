12481287ce425c9b1e08ec6716a0bbb9507a9426
lancewburklin <1677@holbertonschool.com>
2021-05-24 12:01:03 -0500

    Creating an http server

---
+++ 0x12-Node_JS_basic/4-http.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const app = (req, res) => {
+  res.writeHead(200);
+  res.end('Hello Holberton School!');
+};
+
+const server = http.createServer(app);
+server.listen(1245);
