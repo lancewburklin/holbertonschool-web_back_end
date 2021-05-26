9c4be8800ceb6209befad33bb785381b248c800e
lancewburklin <1677@holbertonschool.com>
2021-05-26 13:22:31 -0500

    Adding tests

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -3,9 +3,18 @@ const calculateNumber = require("./0-calcul.js");
 let res = calculateNumber(0, 3);
 describe('calculateNumber', function() {
     it('...', function(){
-        assert.equal(res, 3);
+      assert.equal(res, 3);
     });
     it('...', function(){
-        assert.equal(0, 0);
-    })
+      res = calculateNumber(.3, 5)
+      assert.equal(res, 5);
+    });
+    it('...', function(){
+      res = calculateNumber(2, .5)
+      assert.equal(res, 3);
+    });
+    it('...', function(){
+      res = calculateNumber(.5, .5)
+      assert.equal(res, 2);
+    });
 });
