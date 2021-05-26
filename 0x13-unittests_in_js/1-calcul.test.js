9cc61a92aa6a174a47bb9d314507567b4b4fb1f0
lancewburklin <1677@holbertonschool.com>
2021-05-26 13:37:50 -0500

    Adding tests

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -4,5 +4,21 @@ let res = calculateNumber('SUM', 0, 1);
 describe('SUM', function() {
   it('...', function() {
     assert.equal(res, 1);
-  })
-})
+  });
+});
+describe('SUBTRACT', function() {
+  it('...', function() {
+    res = calculateNumber('SUBTRACT', 3, 2);
+    assert.equal(res, 1);
+  });
+});
+describe('DIVIDE', function() {
+  it('...', function() {
+    res = calculateNumber('DIVIDE', 4, 2);
+    assert.equal(res, 2);
+  });
+  it('...', function() {
+    res = calculateNumber('DIVIDE', 4, 0);
+    assert.equal(res, 'Error');
+  });
+});
