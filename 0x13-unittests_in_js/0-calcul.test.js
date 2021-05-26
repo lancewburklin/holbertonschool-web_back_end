629075089c09ed202e65a2ef748e74a25238ae38
lancewburklin <1677@holbertonschool.com>
2021-05-26 13:17:52 -0500

    Initial commit

---
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const calculateNumber = require("./0-calcul.js");
+let res = calculateNumber(0, 3);
+describe('calculateNumber', function() {
+    it('...', function(){
+        assert.equal(res, 3);
+    });
+    it('...', function(){
+        assert.equal(0, 0);
+    })
+});
---
+++ 0x13-unittests_in_js/README.md
@@ -0,0 +1,1 @@
+Using the JavaScript unittest mosule
