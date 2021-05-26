1e17505e3d51971b6481f4b7e190843637e1d5ea
lancewburklin <1677@holbertonschool.com>
2021-05-26 16:13:24 -0500

    Testing async

--- 0x13-unittests_in_js/6-payment_token.test.js
+++ 0x13-unittests_in_js/6-payment_token.test.js
@@ -1,10 +1,13 @@
+const { assert } = require("chai");
 const getPaymentTokenFromAPI = require("./6-payment_token.js");
 
 describe('getPaymentTokenFromAPI', function() {
   it('...', function(done) {
-    const res = getPaymentTokenFromAPI(true);
-    if (res) {
-      done();
-    }
+    getPaymentTokenFromAPI(true).then(res => {
+      assert.equal(res, {data: 'Successful response from the API' });
+    }).catch(err => {
+      assert.equal(typeof res, 'undefined');
+    });
+    done();
   });
 });
