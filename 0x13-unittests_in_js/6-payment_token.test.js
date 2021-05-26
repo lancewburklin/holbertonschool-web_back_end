932116027eaf7836ee73596f5b973595a43c91e5
lancewburklin <1677@holbertonschool.com>
2021-05-26 16:02:15 -0500

    Asynch testing

---
+++ 0x13-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,10 @@
+const getPaymentTokenFromAPI = require("./6-payment_token.js");
+
+describe('getPaymentTokenFromAPI', function() {
+  it('...', function(done) {
+    const res = getPaymentTokenFromAPI(true);
+    if (res) {
+      done();
+    }
+  });
+});
