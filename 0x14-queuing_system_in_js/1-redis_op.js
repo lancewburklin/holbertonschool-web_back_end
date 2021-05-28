7045d977d8db67ea023acc318a17d0fe84f3d950
lancewburklin <1677@holbertonschool.com>
2021-05-28 10:30:56 -0500

    Change and display Redis key

---
+++ 0x14-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,22 @@
+import redis from 'redis';
+const client = redis.createClient();
+
+client.on('connect', function() {
+  console.log('Redis client connected to the server');
+})
+
+client.on('error', function(err) {
+  console.log(`Redis client not connected to the server: ${err}`);
+})
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, redis.print);
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
